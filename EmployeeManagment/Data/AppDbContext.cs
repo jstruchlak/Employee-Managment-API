48b5f07bd0144b1e611bda9b8fad2fdcd543c778
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-11-19 15:35:05 +1030

    Added db context and CORS policy

---
+++ EmployeeManagment/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagment.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagment.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) 
+        {}
+
+
+    }
+}
