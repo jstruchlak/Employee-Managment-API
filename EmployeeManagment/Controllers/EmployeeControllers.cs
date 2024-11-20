d3cfd8e70b52e2ca73ed9b19d87dbfc01dd3e5a4
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-11-20 10:37:45 +1030

    Configured DI and created API Controller

---
+++ EmployeeManagment/Controllers/EmployeeControllers.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagment.models;
+using EmployeeManagment.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagment.Controller
+{
+    [Route("api[controller]")]
+    [ApiController]
+    public class EmployeeControllers : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeControllers(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+            return Created();
+        }
+    }
+}
